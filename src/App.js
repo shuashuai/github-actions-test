92bfb24f4d1ffb2d69e928e335a9879354b2edcc
shuai <562451625@qq.com>
2022-02-16 12:15:25 +0800

    fix: 文案

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Github Action
+          hello little bitch
         </a>
       </header>
     </div>
